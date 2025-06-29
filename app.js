bb7d7dbfa811e4b90e0c7121bdba569505730e88
Tobias S <tobiascodes12@gmail.com>
2025-06-29 15:45:53 -0400

    Module 8

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const hbs = require('express-handlebars');
 const apiRouter = require('./app_api/routes/index');
 
+
 const app = express();
 
 require("./app_api/models/db")
+require('dotenv').config(); 
+
 
 app.engine(
   'hbs',
@@ -21,7 +24,7 @@ app.engine(
 // Enable CORS 
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Request-Width, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Request-Width, Content-Type, Accept', "Authorization");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 })
@@ -30,8 +33,23 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app_server/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(passport.initialize()); 
 
 app.use('/api', apiRouter);
+
+// Wire in our authentication module 
+var passport = require('passport'); 
+require('./app_api/config/passport'); 
+
+// Catch unauthorized error and create 401 
+app.use((err, req, res, next) => { 
+  if(err.name === 'UnauthorizedError') { 
+    res 
+      .status(401) 
+      .json({"message": err.name + ": " + err.message}); 
+} 
+}); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
